42ec81178cafba3fa1f22005f8747a8c8467a4e9
omkarpai <51873597+omkarpai@users.noreply.github.com>
2019-11-19 19:37:18 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,9 @@
+let displayOut = document.getElementById("outputDisplay");
+let displayIn = document.getElementById("inputDisplay");
+
+displayOut.innerHTML = displayIn.value;
+
+function renderOutput(){
+displayOut.innerHTML = displayIn.value;
+}
+
