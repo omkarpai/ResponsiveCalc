85a701fe359e445abeae87aa05df7a3e3ced9385
omkarpai <51873597+omkarpai@users.noreply.github.com>
2019-11-20 16:04:53 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -2,15 +2,10 @@ let displayOut = document.getElementById("outputDisplay");
 let displayIn = document.getElementById("inputDisplay");
 //let caretPos;
 
-function renderOutput(){
-    displayOut.innerHTML = displayIn.value;
-    let amount=displayIn.value;
-    var numbers = amount.match(/\d*\.*\d+/g).map(Number);
-    var operators = Array.from(amount);
-    Math.evaluate
-    console.log(numbers);
-    console.log(operators);
+function renderOutput(outputValue){
+    displayOut.innerHTML = outputValue;
 }
+   
 
 function insertAtCaret(text) {
     caretPos = displayIn.selectionStart;
@@ -30,7 +25,36 @@ function backspaceCaret() {
     displayIn.focus();
 };
 
+function calcOutput(){
+    expressn=displayIn.value;
+    expressn=expressn.replace('x','*');
+    
+    try{
+    answer = eval(expressn);
+    renderOutput(answer);
+    }
+    
+    catch(error){
+        renderOutput('SyntaxError');
+    }
+
+    if(expressn===""){
+        renderOutput('');
+    }
+}
 
 
 
 
+
+/*Usefull regex
+ let amount=displayIn.value;
+    var numbers = amount.match(/\d*\.*\d+/g).map(Number);
+    let operators = amount.replace(/\d/g,'');
+    operators = operators.replace('.','');
+    var seperated = Array.from(amount);
+    console.log(amount);
+    console.log(numbers);
+    console.log(seperated);
+    console.log(operators);
+*/
