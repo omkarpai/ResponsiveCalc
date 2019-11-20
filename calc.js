0ea680cf9119721e0f2ea0c1380623b536b9af9d
omkarpai <51873597+omkarpai@users.noreply.github.com>
2019-11-20 13:33:30 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,9 +1,36 @@
 let displayOut = document.getElementById("outputDisplay");
 let displayIn = document.getElementById("inputDisplay");
-
-displayOut.innerHTML = displayIn.value;
+//let caretPos;
 
 function renderOutput(){
-displayOut.innerHTML = displayIn.value;
+    displayOut.innerHTML = displayIn.value;
+    let amount=displayIn.value;
+    var numbers = amount.match(/\d*\.*\d+/g).map(Number);
+    var operators = Array.from(amount);
+    Math.evaluate
+    console.log(numbers);
+    console.log(operators);
 }
+
+function insertAtCaret(text) {
+    caretPos = displayIn.selectionStart;
+    var textAreaTxt = displayIn.value;
+    displayIn.value = textAreaTxt.substring(0, caretPos) + text + textAreaTxt.substring(caretPos);
+    displayIn.selectionStart = caretPos +1;
+    displayIn.selectionEnd = caretPos +1;
+    displayIn.focus();
+};
+
+function backspaceCaret() {
+    caretPos = displayIn.selectionStart;
+    var textAreaTxt = displayIn.value;
+    displayIn.value = textAreaTxt.substring(0, caretPos-1) + textAreaTxt.substring(caretPos);
+    displayIn.selectionStart = caretPos - 1;
+    displayIn.selectionEnd = caretPos - 1;
+    displayIn.focus();
+};
+
+
+
+
 
